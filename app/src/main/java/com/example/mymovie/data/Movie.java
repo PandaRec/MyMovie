e690fd599351faae72e99b04dde998a083d55264
Dmitry Pevnitsky <pandarec13@yandex.ru>
2021-01-22 22:40:22 +0300

    class Movie added

--- app/src/main/java/com/example/mymovie/data/Movie.java
+++ app/src/main/java/com/example/mymovie/data/Movie.java
@@ -1,4 +1,99 @@
 package com.example.mymovie.data;
 
 public class Movie {
+    private int id;
+    private String backdropPath;
+    private String originalTitle;
+    private String overview;
+    private String posterPath;
+    private String title;
+    private String releaseDate;
+    private int voteCount;
+    private double voteAverage;
+
+    public Movie(int id, String backdropPath, String originalTitle, String overview,
+                 String posterPath, String title, String releaseDate, int voteCount,
+                 double voteAverage) {
+        this.id = id;
+        this.backdropPath = backdropPath;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.voteCount = voteCount;
+        this.voteAverage = voteAverage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
 }
